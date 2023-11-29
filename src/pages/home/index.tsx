6683d3f465291b914bcd2849d0dfa320e012f7cc
naralicecosta <naralicecosta04@gmail.com>
2023-11-29 11:36:58 -0300

    consumindo api

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 import styles from './home.module.css'
 import  { BiSearch }  from 'react-icons/bi'
-
 import { Link } from 'react-router-dom'
 
+//https://sujeitoprogramador.com/api-cripto/?key=67f9141787211428
 
+interface CoinProps{
+    name: string;
+    delta_24h: string;
+    price: string;
+    symbol: string;
+    volume_24h: string;
+    market_cap: string
+    formatedPrice: string;
+    formatedMarket: string;
+
+}
+interface DataProps{
+    coins: CoinProps[];
+}
 export function Home(){
+    const [coins, setCoins] = useState<CoinProps[]>([])//mostrando que é uma lista e não um objeto
+    console.log(coins)
+
+    useEffect(() => {
+        // criando função e chamando para ser executada
+         function getData(){
+            //fazendo uma requisição
+            fetch('https://sujeitoprogramador.com/api-cripto/?key=67f9141787211428&pref=BRL')
+                .then(response => response.json())
+                .then((data: DataProps)=>{
+                    const coinsData = data.coins.slice(0, 15); //limitando em 15 moedas
+                    
+                    const price = Intl.NumberFormat("pt-BR",{
+                        style: "currency",
+                        currency: 'BRL'
+                    })
+
+                    const formatResult = coinsData.map((item) => {
+                        const formated = {
+                            ...item,
+                            formatedPrice: price.format(Number(item.price)),
+                            formatedMarket: price.format(Number(item.market_cap)),
+                        }
+
+                        return formated
+                    })
+                    setCoins(formatResult)
+
+                })
+            
+
+        }
+        getData();
+     }, [])
     return(
     <>
         {/**area principal do nosso site */}
