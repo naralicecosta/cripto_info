5d37451b84b2e21c116d44059ba6c206e4f821f1
naralicecosta <naralicecosta04@gmail.com>
2023-11-29 14:06:30 -0300

    adicionando rotas

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,7 +1,11 @@
+import styles from './detail.module.css'
+import { useParams } from 'react-router-dom'
+
 export function Detail(){
+    const { cripto } = useParams()
     return(
         <div>
-            <h1>Página Home</h1>
+            <h1>Página Home {cripto}</h1>
         </div>
     )
 }
