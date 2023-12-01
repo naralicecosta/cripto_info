6872fedfb0b8a7a63f19030cba340d77dae898fb
naralicecosta <naralicecosta04@gmail.com>
2023-12-01 11:40:50 -0300

    criando página de erros

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,17 +1,84 @@
-import {useEffect, useState} from 'react'
-
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 import styles from './detail.module.css'
-import { useFetcher, useParams } from 'react-router-dom'
+
+interface CoinProp{
+        symbol: string;
+        name: string;
+        price: string;
+        market_cap: string;
+        low_24h: string;
+        high_24h: string;
+        total_volume_24h: string;
+        delta_24h: string;
+        formatedPrice: string;
+        formatedMarket: string;
+        formatedLowPrice: string;
+        formatedHighprice: string;
+        error?: string;
+    }
+
 
 export function Detail(){
     const { cripto } = useParams()
+    const [detail, setDetail] = useState<CoinProp>()
+    const [loading, setLoading] = useState(true)
 
-    useFetcher(() => {
-        function getData
-    })
+    useEffect(() => {
+        function getData(){
+            fetch(`https://sujeitoprogramador.com/api-cripto/coin/?key=b4cd8f8fb3de94c6&pref=EUR&symbol=${cripto}`)
+            .then(response => response.json())
+            .then((data: CoinProp) => {
+                const price = Intl.NumberFormat("pt-BR",{
+                    style: "currency",
+                    currency: 'BRL'
+                })
+                const resultData = {
+                    ...data,
+                    formatedPrice: price.format(Number(data.price)),
+                    formatedMarket: price.format(Number(data.market_cap)),
+                    formatedLowPrice: price.format(Number(data.low_24h)),
+                    formatedHighprice: price.format(Number(data.high_24h))
+                }
+                setDetail(resultData);
+                setLoading(false);
+            })
+        }
+        getData()
+    },[cripto])
+    
+    if(loading){
+        return(
+            <div className={styles.container}>
+                <h4 className={styles.center}>carregando informações...</h4>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>Página Home {cripto}</h1>
+        <div className={styles.container}>
+            <h1 className={styles.center}>{detail?.name}</h1>
+            <p className={styles.center}>{detail?.symbol}</p>
+            <section className={styles.content}>
+                <p>
+                    <strong>Preço:</strong> {detail?.formatedPrice}
+                </p>
+                <p>
+                    <strong>Maior preço 24h:</strong> {detail?.formatedHighprice}
+                </p>
+                <p>
+                    <strong>Menor Preço 24h:</strong> {detail?.formatedLowPrice}
+                </p>
+                <p>
+                    <strong>Delta 24h:</strong> 
+                    <span className={Number(detail?.delta_24h) >= 0 ? styles.profit : styles.loss}>
+                    {detail?.delta_24h}
+                    </span>
+                </p>
+                <p>
+                    <strong>Valor mercado:</strong> {detail?.formatedMarket}
+                </p>
+            </section>
         </div>
     )
 }
--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,7 +1,14 @@
+import styles from './notfound.module.css'
+import { Link } from 'react-router-dom'
+
 export function NotFoud(){
     return(
-        <div>
+        <div className={styles.container}>
             <h1>Página 404 não existe</h1>
+            <Link to='/'>
+            Acessar cripto moedas
+
+            </Link>
         </div>
     )
 }
